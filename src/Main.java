5bce2490e3fda794bd546c3fa8e28f9586f3b9d2
kymanirjarrett <krj.kymani13@gmail.com>
2024-02-02 12:32:02 -0500

    Initial commit Completed

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int numToExamine = 0;
+        int value = 55;
+
+        if (value % 2 == numToExamine)
+            System.out.println("The value entered is Even");
+        else
+            System.out.println("The value entered is Odd");
+    }
+}
